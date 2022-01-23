3e58615c61b55ea4f7868418e45cdbb36ec4e2c0
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-23 15:53:58 -0500

    Create classes for Status and Guess

--- game.py
+++ game.py
@@ -1,6 +1,41 @@
-import json
-import numpy as np
+from __future__ import annotations
 
+from enum import Enum
+
+import numpy as np
+import json
+
+
+class Status(Enum):
+    GREY = 1
+    YELLOW = 2
+    GREEN = 3
+
+
+class Guess:
+    def __init__(self, word: str):
+        self.word = word
+        self.status_list = [None] * len(word)
+
+    def validate(self, valid_words) -> bool:
+        return self.word in valid_words
+
+    def compute_status_list(self, answer: str) -> list[Status]:
+        for guess_idx, guess_letter in enumerate(self.word):
+            answer_indices = []
+
+            answer_idx = answer.find(guess_letter)
+            while answer_idx != -1:
+                answer_indices.append(answer_idx)
+                answer_idx = answer.find(guess_letter, start=answer_idx + 1)
+            
+            if len(answer_indices) == 0:
+                self.status_list[guess_idx] = Status.GREY
+            elif guess_idx in answer_indices:
+                self.status_list[guess_idx] = Status.GREEN
+            else:
+                self.status_list[guess_idx] = Status.YELLOW
+        return self.status_list
 
 def load_words(length: int = None) -> None:
     words = None
@@ -30,6 +65,7 @@ def pick_word(words: set) -> str:
 
 def main():
     length_choice = 5
+    num_guesses = 6
 
     words = load_words(length=length_choice)
     answers = load_answers(length=length_choice)
@@ -37,6 +73,9 @@ def main():
     answer = pick_word(answers)
 
     print(answer)
+
+    board = [[None] * length_choice] * num_guesses
+    print(board)
 
 
 if __name__ == '__main__':
