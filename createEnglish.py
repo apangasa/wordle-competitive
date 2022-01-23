9f091aadeb2a93f9961c39ab446c21b474fe8485
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-23 14:44:20 -0500

    Add filtering of words for particular lengths

--- createEnglish.py
+++ createEnglish.py
@@ -1,12 +1,22 @@
+from __future__ import annotations
+
 from nltk import corpus  # must download words
 import english_words
 
 import json
 
 
-def populate_words(current_words: set, new_words: set):
+DEFAULT_LENGTHS = [5, 6, 7]
+
+
+def populate_words(current_words: set, new_words: set) -> set:
     current_words.update(new_words)
     return current_words
+
+
+def write_to_json(fname: str, words: list[str]) -> None:
+    with open(fname, 'w') as file:
+        json.dump(words, file)
 
 
 def main():
@@ -18,8 +28,11 @@ def main():
     # Add words from https://svnweb.freebsd.org/csrg/share/dict/
     words = populate_words(words, set(english_words.english_words_set))
 
-    with open('./valid_words.json', 'w') as word_file:
-        json.dump(list(words), word_file)
+    write_to_json('./valid_words.json', list(words))
+
+    for length in DEFAULT_LENGTHS:
+        filtered_words = filter(lambda word: len(word) == length, words)
+        write_to_json(f'./valid_words_{length}.json', list(filtered_words))
 
 
 if __name__ == '__main__':
