074c7f37b7455262cea7674cebd52f74a0bd92f5
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-23 15:17:20 -0500

    Identify well-known words as potential answers

--- createEnglish.py
+++ createEnglish.py
@@ -3,10 +3,19 @@ from __future__ import annotations
 from nltk import corpus  # must download words
 import english_words
 
+import wordfreq
+
 import json
 
 
 DEFAULT_LENGTHS = [5, 6, 7]
+
+# Minimum Zipf frequency for a word to be a possible answer
+FREQUENCY_THRESHOLD = 4.0
+
+
+def frequency_is_above(word: str, frequency_threshold: float = FREQUENCY_THRESHOLD) -> bool:
+    return wordfreq.zipf_frequency(word, 'en') >= frequency_threshold
 
 
 def populate_words(current_words: set, new_words: set) -> set:
@@ -15,6 +24,8 @@ def populate_words(current_words: set, new_words: set) -> set:
 
 
 def write_to_json(fname: str, words: list[str]) -> None:
+    if len(words) == 0:
+        print(fname, words)
     with open(fname, 'w') as file:
         json.dump(words, file)
 
@@ -29,12 +40,18 @@ def main():
     words = populate_words(words, set(english_words.english_words_set))
 
     words = set(map(lambda word: word.upper(), words))
+    answers = set(filter(lambda word: frequency_is_above(word), words))
 
     write_to_json('./valid_words.json', list(words))
+    write_to_json('./answers.json', list(answers))
 
     for length in DEFAULT_LENGTHS:
-        filtered_words = filter(lambda word: len(word) == length, words)
+        filtered_words = list(filter(lambda word: len(word) == length, words))
+        filtered_answers = list(
+            filter(lambda word: frequency_is_above(word), filtered_words))
+
         write_to_json(f'./valid_words_{length}.json', list(filtered_words))
+        write_to_json(f'./answers_{length}.json', list(filtered_answers))
 
 
 if __name__ == '__main__':
