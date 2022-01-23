042fe4eed3a0be461d9cd15fcd1e952b1704c5ee
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-23 16:26:58 -0500

    Create GuessLetter class and implement guessing

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ from enum import Enum
 import numpy as np
 import json
 
+from pprint import pprint
+
 
 class Status(Enum):
     GREY = 1
@@ -12,30 +14,47 @@ class Status(Enum):
     GREEN = 3
 
 
+class GuessLetter:
+    def __init__(self, letter: str, status: Status):
+        self.letter = letter
+        self.status = status
+
+    def __str__(self):
+        return f'{self.letter}, {self.status}'
+
+    def __repr__(self):
+        return f'{self.letter}, {self.status}'
+
+
 class Guess:
-    def __init__(self, word: str):
+    def __init__(self, word: str, answer: str):
         self.word = word
-        self.status_list = [None] * len(word)
+        self.answer = answer
+        self.guess_letters = [None] * len(word)
+        self.compute_statuses()
 
     def validate(self, valid_words) -> bool:
         return self.word in valid_words
 
-    def compute_status_list(self, answer: str) -> list[Status]:
+    def compute_statuses(self) -> None:
         for guess_idx, guess_letter in enumerate(self.word):
             answer_indices = []
 
-            answer_idx = answer.find(guess_letter)
+            answer_idx = self.answer.find(guess_letter)
             while answer_idx != -1:
                 answer_indices.append(answer_idx)
-                answer_idx = answer.find(guess_letter, start=answer_idx + 1)
-            
+                answer_idx = self.answer.find(guess_letter, answer_idx + 1)
+
             if len(answer_indices) == 0:
-                self.status_list[guess_idx] = Status.GREY
+                self.guess_letters[guess_idx] = GuessLetter(
+                    guess_letter, Status.GREY)
             elif guess_idx in answer_indices:
-                self.status_list[guess_idx] = Status.GREEN
+                self.guess_letters[guess_idx] = GuessLetter(
+                    guess_letter, Status.GREEN)
             else:
-                self.status_list[guess_idx] = Status.YELLOW
-        return self.status_list
+                self.guess_letters[guess_idx] = GuessLetter(
+                    guess_letter, Status.YELLOW)
+
 
 def load_words(length: int = None) -> None:
     words = None
@@ -65,17 +84,26 @@ def pick_word(words: set) -> str:
 
 def main():
     length_choice = 5
-    num_guesses = 6
+    guesses_left = num_guesses = 6
 
     words = load_words(length=length_choice)
     answers = load_answers(length=length_choice)
 
     answer = pick_word(answers)
 
-    print(answer)
+    # print(answer)
 
     board = [[None] * length_choice] * num_guesses
-    print(board)
+    pprint(board)
+
+    while guesses_left:
+        guess = Guess(input('Enter guess: ').upper(), answer)
+        board[num_guesses - guesses_left] = guess.guess_letters
+        guesses_left -= 1
+
+        pprint(board)
+
+    print(answer)
 
 
 if __name__ == '__main__':
