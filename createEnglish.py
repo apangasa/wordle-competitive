8ccf4a6ab12b3c42eedc9a2d3c5a2464e908d4a9
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-23 14:26:19 -0500

    Update populate words function

--- createEnglish.py
+++ createEnglish.py
@@ -1,17 +1,21 @@
-from nltk import corpus
+from nltk import corpus  # must download words
+import english_words
 
 import json
 
 
-def populate_words_nltk(words: set):
-    words.update(set(corpus.words.words()))
+def populate_words(current_words: set, new_words: set):
+    current_words.update(new_words)
     return words
 
 
 if __name__ == '__main__':
     words = set()
 
-    words = populate_words_nltk(words)
+    # Add words from NLTK WordNet
+    words = populate_words(words, set(corpus.words.words()))
+
+    # Add words from https://svnweb.freebsd.org/csrg/share/dict/
 
     with open('./valid_words.json', 'w') as word_file:
         json.dump(list(words), word_file)
